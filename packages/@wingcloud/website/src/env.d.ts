38efb8125b09bc7268a3e1705e1987008f35d7be
Cristian Pallarés <cristianp@wing.cloud>
2023-09-07 15:36:33 +0200

    refactor: vite plugin to astro plugin
    Also, adds a favicon to wing.cloud.

--- packages/@wingcloud/website/src/env.d.ts
+++ packages/@wingcloud/website/src/env.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="astro/client" />
 
-declare module "virtual:@wingcloud/vite-dynamodb-plugin" {
+declare module "virtual:@wingcloud/astro-dynamodb-integration" {
   export const client: import("@aws-sdk/client-dynamodb").DynamoDBClient;
 
   export const TableName: string;
