e3ef286e21c48ec41762d5cf9bcb1b4148938dcb
Cristian Pallarés <cristianp@wing.cloud>
2023-09-07 12:07:43 +0200

    feat: add vite-dynamodb-plugin

--- packages/@wingcloud/website/src/env.d.ts
+++ packages/@wingcloud/website/src/env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="astro/client" />
+
+declare module "virtual:@wingcloud/vite-dynamodb-plugin" {
+  export const client: import("@aws-sdk/client-dynamodb").DynamoDBClient;
+
+  export const TableName: string;
+}
