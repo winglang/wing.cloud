9f1475c11924d8b18baf8e887314514694ced838
Cristian Pallarés <cristianp@wing.cloud>
2023-09-08 14:17:30 +0200

    wip

--- packages/@wingcloud/website/src/pages/trpc/[trpc].ts
+++ packages/@wingcloud/website/src/pages/trpc/[trpc].ts
@@ -1,8 +1,8 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import type { APIRoute } from "astro";
 
-import { createContext } from "../../router/context.js";
-import { appRouter } from "../../router/index.js";
+import { createContext } from "../../routers/context.js";
+import { appRouter } from "../../routers/index.js";
 
 export const all: APIRoute = ({ request }) => {
   return fetchRequestHandler({
