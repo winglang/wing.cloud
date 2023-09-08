8191dc2fdef7b04ce2fb3a42f644f22a55dbc8bb
Cristian Pallarés <cristianp@wing.cloud>
2023-09-08 10:35:42 +0200

    chore(astro-dynamodb-integration): skip pulling images if possible
    Additionally:
    
    - Generate a `shim.d.ts` inside of the project's `.wing/` directory
    - Log to the debug channel instead of the info channel

--- packages/@wingcloud/website/src/env.d.ts
+++ packages/@wingcloud/website/src/env.d.ts
@@ -1,7 +1,1 @@
 /// <reference types="astro/client" />
-
-declare module "virtual:@wingcloud/astro-dynamodb-integration" {
-  export const client: import("@aws-sdk/client-dynamodb").DynamoDBClient;
-
-  export const TableName: string;
-}
