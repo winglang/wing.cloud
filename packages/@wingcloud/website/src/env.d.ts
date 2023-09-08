eeb52b44130e21df15943a9c1a1177defc95549d
polamoros <info@polamoros.com>
2023-09-08 11:18:26 +0200

    wip

--- packages/@wingcloud/website/src/env.d.ts
+++ packages/@wingcloud/website/src/env.d.ts
@@ -1,5 +1,14 @@
 /// <reference types="astro/client" />
 
+interface ImportMetaEnv {
+  readonly GITHUB_APP_CLIENT_ID: string;
+  readonly GITHUB_APP_CLIENT_SECRET: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
 declare module "virtual:@wingcloud/vite-dynamodb-plugin" {
   export const client: import("@aws-sdk/client-dynamodb").DynamoDBClient;
 
