635d848f755fb5a251a085d359503c8259f29080
Cristian Pallarés <cristianp@wing.cloud>
2023-09-08 16:58:18 +0200

    wip

--- packages/@wingcloud/astro/src/index.ts
+++ packages/@wingcloud/astro/src/index.ts
@@ -2,10 +2,5 @@ import type { AstroIntegration } from "astro";
 
 import { dynamodb } from "./dynamodb-integration.js";
 import { environmentTypes } from "./environment-types-integration.js";
-import { virtualModuleTypes } from "./virtual-module-types-integration.js";
 
-export default (): AstroIntegration[] => [
-  dynamodb(),
-  environmentTypes(),
-  virtualModuleTypes(),
-];
+export default (): AstroIntegration[] => [dynamodb(), environmentTypes()];
