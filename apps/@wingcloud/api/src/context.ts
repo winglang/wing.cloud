99333a7a4fd97b6006c91e63b2d2057c2545d2ce
Cristian Pallarés <cristianp@wing.cloud>
2023-09-12 17:14:02 +0200

    wip

--- apps/@wingcloud/api/src/context.ts
+++ apps/@wingcloud/api/src/context.ts
@@ -1,15 +1,4 @@
 import { DynamoDB } from "@aws-sdk/client-dynamodb";
-import type { inferAsyncReturnType } from "@trpc/server";
-import type { CreateExpressContextOptions } from "@trpc/server/adapters/express";
-
-// export const createContext = ({}: CreateExpressContextOptions) => {
-//   const dynamodb = new DynamoDB({});
-//   return {
-//     dynamodb,
-//   };
-// };
-
-// export type Context = inferAsyncReturnType<typeof createContext>;
 
 export type Context = {
   dynamodb: DynamoDB;
