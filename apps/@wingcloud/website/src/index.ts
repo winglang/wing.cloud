d10be6d5fe198824f88e3ef6794f88c71062af5c
Cristian Pallarés <cristianp@wing.cloud>
2023-09-13 15:02:35 +0200

    wip

--- apps/@wingcloud/website/src/index.ts
+++ apps/@wingcloud/website/src/index.ts
@@ -1,7 +1,7 @@
 import { StrictMode, createElement } from "react";
 import { createRoot } from "react-dom/client";
 
-import { App } from "./app.js";
+import { App } from "./app.jsx";
 
 createRoot(document.querySelector("#app")!).render(
   createElement(StrictMode, undefined, createElement(App)),
