1623991660508cfb672cc55235bde357d78b2715
Cristian Pallarés <cristianp@wing.cloud>
2023-09-13 10:21:27 +0200

    chore(api): add request and response to context

--- apps/@wingcloud/api/src/context.ts
+++ apps/@wingcloud/api/src/context.ts
@@ -1,6 +1,32 @@
 import { type DynamoDB } from "@aws-sdk/client-dynamodb";
+import { type CreateExpressContextOptions } from "@trpc/server/adapters/express";
+
+import type { UserId } from "./types/user.js";
 
 export type Context = {
   dynamodb: DynamoDB;
   tableName: string;
+  request: CreateExpressContextOptions["req"];
+  response: CreateExpressContextOptions["res"];
+  userId?: UserId;
+};
+
+export type BuildCreateContextOptions = {
+  dynamodb: DynamoDB;
+  tableName: string;
+};
+
+export const buildCreateContext = (options: BuildCreateContextOptions) => {
+  return ({
+    req: request,
+    res: response,
+  }: CreateExpressContextOptions): Context => {
+    // TODO: Get userId from JWT cookie.
+    return {
+      dynamodb: options.dynamodb,
+      tableName: options.tableName,
+      request,
+      response,
+    };
+  };
 };
