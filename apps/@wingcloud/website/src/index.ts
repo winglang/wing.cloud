5ab7b32a6891df1bb80b5fdf9380e283a1836f60
Cristian Pallarés <cristianp@wing.cloud>
2023-09-13 15:02:53 +0200

    chore: upgrade wingen (#33)

--- apps/@wingcloud/website/src/index.ts
+++ apps/@wingcloud/website/src/index.ts
@@ -1,6 +1,8 @@
-import { createElement } from "react";
+import { StrictMode, createElement } from "react";
 import { createRoot } from "react-dom/client";
 
 import { App } from "./app.jsx";
 
-createRoot(document.querySelector("#app")!).render(createElement(App));
+createRoot(document.querySelector("#app")!).render(
+  createElement(StrictMode, undefined, createElement(App)),
+);
