8a92ec35b5faca1876696e9e7a759ae836e9c2e4
Cristian Pallarés <cristianp@wing.cloud>
2023-10-31 12:43:46 +0100

    wip

--- apps/@wingcloud/runtime/src/entrypoint-flyio.ts
+++ apps/@wingcloud/runtime/src/entrypoint-flyio.ts
@@ -4,46 +4,25 @@ import { handler } from "./index.js";
 
 const require = createRequire(import.meta.url);
 
-const targetOptions =
-  process.env["WING_TARGET"] == "tf-aws"
-    ? {
-        directory: "shared-aws",
-        bucketClass: "BucketClient",
-        arguments: [process.env["LOGS_BUCKET_NAME"]],
-      }
-    : {
-        directory: "target-sim",
-        bucketClass: "Bucket",
-        arguments: [
-          { topics: {} },
-          {
-            withTrace: async ({
-              message,
-              activity,
-            }: {
-              message: string;
-              activity: () => Promise<any>;
-            }) => {
-              console.log(message);
-              return activity();
-            },
-          },
-        ],
-      };
+const targetOptions = process.env["WING_TARGET"] == "tf-aws" ? {
+  directory: "shared-aws",
+  bucketClass: "BucketClient",
+  arguments: [process.env["LOGS_BUCKET_NAME"]]
+} : {
+  directory: "target-sim",
+  bucketClass: "Bucket",
+  arguments: [{ topics: {} }, { withTrace: async ({ message, activity }: { message: string, activity: () => Promise<any> }) => {
+    console.log(message);
+    return activity();
+  }}]
+};
 
-const BucketInflight = require(
-  join(
-    dirname(require.resolve("@winglang/sdk")),
-    `${targetOptions.directory}/bucket.inflight`,
-  ),
-);
+const BucketInflight = require(join(dirname(require.resolve("@winglang/sdk")), `${targetOptions.directory}/bucket.inflight`));
 const bucketName = process.env["LOGS_BUCKET_NAME"];
 if (!bucketName) {
   throw new Error("missing bucket name");
 }
-const logsBucket = new BucketInflight[targetOptions.bucketClass](
-  ...targetOptions.arguments,
-);
+const logsBucket = new BucketInflight[targetOptions.bucketClass](...targetOptions.arguments);
 
 const wingApiUrl = process.env["WING_CLOUD_URL"];
 if (!wingApiUrl) {
@@ -51,7 +30,7 @@ if (!wingApiUrl) {
 }
 
 try {
-  new URL(wingApiUrl);
+  new URL(wingApiUrl)
   console.log("url ok");
 } catch (e: any) {
   console.log("url no ok", e.toString());
