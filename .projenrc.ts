67806f8da625283fd65c3a28aba992932caf3bad
Sebastian Korfmann <sebastian@korfmann.net>
2023-11-06 23:33:29 +0100

    chore: fly app names are globally unique (#102)
    https://community.fly.io/t/are-app-names-in-a-global-namespace/4819

--- .projenrc.ts
+++ .projenrc.ts
@@ -327,6 +327,7 @@ infrastructure.addDeps(
   "@cdktf/provider-dnsimple",
   "@cdktf/provider-docker",
   "@cdktf/provider-null",
+  "@cdktf/provider-random",
 );
 
 infrastructure.addDevDeps("@types/cookie");
