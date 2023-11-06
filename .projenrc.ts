29d154d7d81192aa2d7b23ea2da30e6c97ead9e8
Sebastian Korfmann <sebastian@korfmann.net>
2023-11-06 13:25:06 +0100

    chore: aws basic setup (#89)
    See
    [readme](https://github.com/winglang/wing.cloud/blob/325f09c8b1b25f4698d8e319cdd9aaee04590520/aws/README.md)
    
    Already applied in the mentioned accounts.

--- .projenrc.ts
+++ .projenrc.ts
@@ -338,4 +338,21 @@ infrastructure.addDevDeps(flyio.name);
 infrastructure.addDevDeps(runtime.name);
 
 ///////////////////////////////////////////////////////////////////////////////
+
+const aws = new NodeProject({
+  parent: monorepo,
+  name: "@wingcloud/aws-cicd",
+  outdir: "aws",
+});
+
+aws.addDeps("@cdktf/provider-aws");
+aws.addDeps("cdktf");
+aws.addDeps("constructs");
+aws.addGitIgnore("**/target/");
+aws.addFields({
+  type: "commonjs",
+})
+
+///////////////////////////////////////////////////////////////////////////////
+
 monorepo.synth();
