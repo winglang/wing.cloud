00863ca35150350ad74f3be38ec2ef21a750fb00
Cristian Pallarés <cristianp@wing.cloud>
2023-11-22 13:36:33 +0100

    fix vertical space

--- apps/@wingcloud/website/src/routes/new/index.tsx
+++ apps/@wingcloud/website/src/routes/new/index.tsx
@@ -16,22 +16,25 @@ export const Component = () => {
     <NewAppContainer>
       <div className="w-full space-y-4">
         <div className={clsx(theme.text1)}>Select a configuration</div>
-        <AppConfigurationListItem
-          name="Connect"
-          description="Connect to an existing repository"
-          icon={<LinkIcon className="w-5 h-5" />}
-          onClick={() => {
-            navigate("/apps/new/connect/");
-          }}
-        />
-        <AppConfigurationListItem
-          name=""
-          description="More coming soon!"
-          icon={<SquaresPlusIcon className="w-5 h-5" />}
-          disabled={true}
-          classname="opacity-50 cursor-default"
-        />
-        <div className="w-full flex pt-4">
+        <div className="space-y-2">
+          <AppConfigurationListItem
+            name="Connect"
+            description="Connect to an existing repository"
+            icon={<LinkIcon className="w-5 h-5" />}
+            onClick={() => {
+              navigate("/apps/new/connect/");
+            }}
+          />
+          <AppConfigurationListItem
+            name=""
+            description="More coming soon!"
+            icon={<SquaresPlusIcon className="w-5 h-5" />}
+            disabled={true}
+            classname="opacity-50 cursor-default"
+          />
+        </div>
+
+        <div className="w-full flex">
           <div className="justify-end flex gap-x-2 grow">
             <Button
               onClick={() => {
