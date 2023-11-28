0b217232784a6a739121fc68749746db4cb575fb
Cristian Pallarés <cristianp@wing.cloud>
2023-11-28 09:50:45 +0100

    wip

--- apps/@wingcloud/website/src/layout/layout.tsx
+++ apps/@wingcloud/website/src/layout/layout.tsx
@@ -3,12 +3,8 @@ import { useMemo, type PropsWithChildren } from "react";
 import { useLocation } from "react-router-dom";
 
 import { Header } from "../components/header.js";
-import { SpinnerLoader } from "../components/spinner-loader.js";
-import { wrpc } from "../utils/wrpc.js";
 
 export const Layout = ({ children }: PropsWithChildren) => {
-  let authCheck;
-
   const location = useLocation();
 
   const fullWidthPage = useMemo(() => {
@@ -19,39 +15,19 @@ export const Layout = ({ children }: PropsWithChildren) => {
     return false;
   }, [location.pathname]);
 
-  if (location.pathname !== "/") {
-    try {
-      authCheck = wrpc["auth.check"].useQuery(undefined, {
-        throwOnError: true,
-        retry: false,
-      });
-    } catch (error) {
-      console.log(error);
-      window.location.href = "/";
-    }
-  }
-
   return (
     <>
-      {authCheck?.isLoading && (
-        <div className="fixed inset-0 flex items-center justify-center">
-          <SpinnerLoader />
-        </div>
-      )}
-
       <div className="absolute inset-0 flex flex-col">
-        {authCheck?.data?.userId && <Header />}
+        <Header />
 
-        {!authCheck?.isLoading && (
-          <div
-            className={clsx(
-              "w-full flex-grow overflow-auto",
-              !fullWidthPage && "max-w-5xl mx-auto py-4 px-4 sm:px-6 sm:py-6",
-            )}
-          >
-            {children}
-          </div>
-        )}
+        <div
+          className={clsx(
+            "w-full flex-grow overflow-auto",
+            !fullWidthPage && "max-w-5xl mx-auto py-4 px-4 sm:px-6 sm:py-6",
+          )}
+        >
+          {children}
+        </div>
       </div>
     </>
   );
