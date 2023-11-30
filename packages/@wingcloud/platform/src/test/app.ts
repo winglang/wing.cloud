9415476ac9639b363f8aa01d0e0b31eacfad894e
Sebastian Korfmann <sebastian@korfmann.net>
2023-11-30 21:42:12 +0100

    align with upstream implementation

--- packages/@wingcloud/platform/src/test/app.ts
+++ packages/@wingcloud/platform/src/test/app.ts
@@ -1,3 +1,4 @@
+import { Node } from '@winglang/sdk/lib/std';
 import * as tfaws from '@winglang/sdk/lib/target-tf-aws';
 import type { AppProps } from '@winglang/sdk/lib/core';
 import type { TestRunner } from '@winglang/sdk/lib/std';
@@ -8,12 +9,18 @@ export class App extends tfaws.App {
  * to allow for a single root construct, rather than one per
  * test.
  *
+ * https://github.com/winglang/wing/blob/main/libs/wingsdk/src/core/app.ts
+ *
  * @param props The App props
  * @param testRunner The test runner
  */
   protected synthRoots(props: AppProps, testRunner: TestRunner) {
     if (props.rootConstruct) {
       const Root = props.rootConstruct;
+      // mark the root type so that we can find it later through
+      // Node.of(root).root
+      Node._markRoot(Root);
+
       new Root(this, "Default");
     }
   }
