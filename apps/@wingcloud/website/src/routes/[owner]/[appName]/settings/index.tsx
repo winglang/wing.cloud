693312aa7a69fe8794abc7b3fcaf9ea29b6a1c45
Cristian Pallarés <cristianp@wing.cloud>
2023-12-02 10:43:14 +0100

    fix: standardize gray colors to `slate`
    The Console uses `slate` as the main gray color, and Wing Cloud uses a mix of `gray` and `slate`. Now, `slate` is used for all gray colors.

--- apps/@wingcloud/website/src/routes/[owner]/[appName]/settings/index.tsx
+++ apps/@wingcloud/website/src/routes/[owner]/[appName]/settings/index.tsx
@@ -156,7 +156,7 @@ const SettingsPage = () => {
                 </Button>
               </div>
             </div>
-            <hr className="h-px mt-4 mb-2 bg-gray-200 border-0 dark:bg-gray-700" />
+            <hr className="h-px mt-4 mb-2 bg-slate-200 border-0 dark:bg-slate-700" />
             <div className="flex flex-row gap-2">
               <ExclamationCircleIcon
                 className={clsx("h-4 w-4 flex-shrink-0", theme.text2)}
