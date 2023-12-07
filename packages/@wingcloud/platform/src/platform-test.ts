9312cc9cd62c3eba01dbf2001fbe094989642a6d
Sebastian Korfmann <sebastian@korfmann.net>
2023-12-07 10:10:47 +0100

    chore: testing workflow (#188)
    This adds a testing workflow in Github Actions. It'll 
    
    - ~run simulator tests for the application~ (extracted to
    https://github.com/winglang/wing.cloud/pull/263)
    - run aws tests for the application 
    
    Unfortunately, concurrency of the test runs can't be configured properly
    across the repo within Github Actions. So we might run into issues
    around the Github Application. But let's see.
    
    note from diff:
    
    - this adds basic instrumentation for api gateway and lambda
    - removes testing environments from the deployed infrastructure 
    
    ~Still todo:~ 
    
    - [ ] ~Run all wing related tests, also for dedicated and embedded
    modules~ https://github.com/winglang/wing.cloud/issues/324
    - [ ] ~Add wing simulator tests as part of `main` deploy pipeline~
    https://github.com/winglang/wing.cloud/pull/263
    - [ ] ~Fix existing tests~
    https://github.com/winglang/wing.cloud/issues/322
    - [ ] ~Add a few more tests~
    - [ ] ~Experiment with
    [checks](https://github.com/winglang/winglibs/tree/main/checks)~
    https://github.com/winglang/wing.cloud/issues/323

---
+++ packages/@wingcloud/platform/src/platform-test.ts
@@ -0,0 +1,13 @@
+import { type ICustomPlatform } from './platform';
+import s3Backend from './s3-backend';
+import awsS3Bucket from './test/aws_s3_bucket';
+import awsDynamodbTable from './test/aws_dynamodb_table';
+
+export class TestPlatform implements ICustomPlatform {
+  postSynth(config: any) {
+    config = s3Backend(config);
+    config = awsS3Bucket(config);
+    config = awsDynamodbTable(config);
+    return config;
+  }
+}
---
+++ packages/@wingcloud/platform/tsup.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  "entry": [
+    "src/**/*.ts"
+  ],
+  "outDir": "lib",
+  "format": [
+    "cjs"
+  ],
+  "target": "node18",
+  "dts": true,
+  "bundle": false,
+  "clean": true
+});
