c43255fc466fa727a7e9baf9c5c855873bdf6410
Ainvoner <shaia@monada.co>
2024-02-01 09:48:53 +0200

    chore: add data store for user apps
    The motivation for this change was that newly added apps weren't immediately shown when the user navigated back to the apps list page.
    
    The reason was that although the app.list query was invalidated, the refetch happened only when the user was in this page.
    
    having the apps list data at the top level of the application and not tied to a specific component allows us to reload the data from anywhere in our application.

---
+++ apps/@wingcloud/website/src/data-store/apps-data-provider.tsx
@@ -0,0 +1,61 @@
+import {
+  createContext,
+  type PropsWithChildren,
+  useEffect,
+  useState,
+} from "react";
+
+import type { App } from "../utils/wrpc.js";
+import { wrpc } from "../utils/wrpc.js";
+
+export interface AppsDataProviderContext {
+  apps: App[];
+  isLoading: boolean;
+  isError: boolean;
+}
+const DEFAULT_CONTEXT: AppsDataProviderContext = {
+  apps: [],
+  isLoading: true,
+  isError: false,
+};
+export const AppsDataProviderContext =
+  createContext<AppsDataProviderContext>(DEFAULT_CONTEXT);
+
+export const AppsDataProvider = ({ children }: PropsWithChildren) => {
+  const [apps, setApps] = useState<App[]>([]);
+  const user = wrpc["auth.check"].useQuery(undefined, {
+    throwOnError: false,
+    retry: false,
+  });
+  const listAppsQuery = wrpc["app.list"].useQuery(
+    {
+      owner: user?.data?.user.username!,
+    },
+    {
+      enabled: !!user?.data?.user.username,
+    },
+  );
+
+  // trigger initial fetch only if owner is set
+  useEffect(() => {
+    if (!user?.data?.user.username) return;
+    listAppsQuery.refetch();
+  }, [user?.data?.user.username]);
+
+  useEffect(() => {
+    if (!listAppsQuery.data) return;
+    setApps(listAppsQuery.data.apps);
+  }, [listAppsQuery.data]);
+
+  return (
+    <AppsDataProviderContext.Provider
+      value={{
+        apps,
+        isLoading: listAppsQuery.isLoading,
+        isError: listAppsQuery.isError,
+      }}
+    >
+      {children}
+    </AppsDataProviderContext.Provider>
+  );
+};
