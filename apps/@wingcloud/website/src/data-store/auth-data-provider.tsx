92072a5eb77c5f5c8ea78e78250e6530ce9da4f0
polamoros <info@polamoros.com>
2024-02-06 17:04:38 +0100

    wip

---
+++ apps/@wingcloud/website/src/data-store/auth-data-provider.tsx
@@ -0,0 +1,33 @@
+import { createContext, type PropsWithChildren } from "react";
+
+import type { App, User } from "../utils/wrpc.js";
+import { wrpc } from "../utils/wrpc.js";
+
+export interface AuthDataProviderContext {
+  user?: User;
+  error?: Error;
+}
+const DEFAULT_CONTEXT: AuthDataProviderContext = {
+  user: undefined,
+  error: undefined,
+};
+export const AuthDataProviderContext =
+  createContext<AuthDataProviderContext>(DEFAULT_CONTEXT);
+
+export const AuthDataProvider = ({ children }: PropsWithChildren) => {
+  const user = wrpc["auth.check"].useQuery(undefined, {
+    throwOnError: false,
+    retry: false,
+  });
+
+  return (
+    <AuthDataProviderContext.Provider
+      value={{
+        user: user.data?.user,
+        error: user.error ?? undefined,
+      }}
+    >
+      {children}
+    </AuthDataProviderContext.Provider>
+  );
+};
