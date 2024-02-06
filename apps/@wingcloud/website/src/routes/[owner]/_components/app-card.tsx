09c322dd7c28be3c81c3d569d69144560fd10b6d
polamoros <info@polamoros.com>
2024-02-06 08:55:38 +0100

    wip

--- apps/@wingcloud/website/src/routes/[owner]/_components/app-card.tsx
+++ apps/@wingcloud/website/src/routes/[owner]/_components/app-card.tsx
@@ -25,7 +25,7 @@ export const AppCard = ({ app, owner }: { app: App; owner: string }) => {
 
   const branchUrl = useMemo(() => {
     if (!app.defaultBranch) {
-      return `https://github.com/${app.repoOwner}/${app.repoName}/tree`;
+      return `https://github.com/${app.repoOwner}/${app.repoName}`;
     }
     return `https://github.com/${app.repoOwner}/${app.repoName}/tree/${app.defaultBranch}`;
   }, [app]);
