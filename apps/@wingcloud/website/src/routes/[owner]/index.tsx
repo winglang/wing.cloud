fa3cd2a0bb2b07ce7990c28ce4333ea92c07a327
polamoros <info@polamoros.com>
2024-02-07 13:14:38 +0100

    wip

--- apps/@wingcloud/website/src/routes/[owner]/index.tsx
+++ apps/@wingcloud/website/src/routes/[owner]/index.tsx
@@ -56,7 +56,7 @@ const OwnerPage = () => {
       )
       .sort((a, b) => {
         return (
-          new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+          new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
         );
       });
   }, [apps, search]);
