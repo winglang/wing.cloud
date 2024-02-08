84ada0a38f1fe5c32d4ab6557b733f5ed79ed4a8
polamoros <info@polamoros.com>
2024-02-08 15:30:28 +0100

    wip

--- apps/@wingcloud/website/src/components/status-pill.tsx
+++ apps/@wingcloud/website/src/components/status-pill.tsx
@@ -21,20 +21,28 @@ export const StatusPill = ({
   return (
     <div
       className={clsx(
-        status === "initializing" &&
-          "text-yellow-600 bg-yellow-100 animate-pulse",
-        status === "running-tests" &&
-          "text-yellow-600 bg-yellow-100 animate-pulse",
-        status === "running-server" &&
-          "text-yellow-600 bg-yellow-100 animate-pulse",
-        status === "deploying" && "text-yellow-600 bg-yellow-100 animate-pulse",
-        status === "running" && "text-green-700 bg-green-100",
-        status === "error" && "text-red-600 bg-red-100",
-        "text-xs rounded-xl px-2 py-0.5",
-        "capitalize font-[500]",
+        status === "initializing" && "bg-yellow-100",
+        status === "running-tests" && "bg-yellow-100",
+        status === "running-server" && "bg-yellow-100",
+        status === "deploying" && "bg-yellow-100",
+        status === "running" && "bg-green-100",
+        status === "error" && "bg-red-100",
+        "rounded-xl px-2 py-0.5",
       )}
     >
-      {children || statusString}
+      <div
+        className={clsx(
+          status === "initializing" && "text-yellow-600 animate-pulse",
+          status === "running-tests" && "text-yellow-600 animate-pulse",
+          status === "running-server" && "text-yellow-600 animate-pulse",
+          status === "deploying" && "text-yellow-600 animate-pulse",
+          status === "running" && "text-green-700",
+          status === "error" && "text-red-600",
+          "text-xs capitalize font-[500]",
+        )}
+      >
+        {children || statusString}
+      </div>
     </div>
   );
 };
