7dac445372e5f93a7faf28ab4624d463713ff0fb
polamoros <info@polamoros.com>
2024-02-09 09:49:44 +0100

    wip

--- apps/@wingcloud/website/src/routes/[owner]/[appName]/index.tsx
+++ apps/@wingcloud/website/src/routes/[owner]/[appName]/index.tsx
@@ -4,6 +4,7 @@ import { useNavigate, useParams } from "react-router-dom";
 
 import { ErrorBoundary } from "../../../components/error-boundary.js";
 import { Header } from "../../../components/header.js";
+import { PageHeader } from "../../../components/page-header.js";
 import { Button } from "../../../design-system/button.js";
 import { SkeletonLoader } from "../../../design-system/skeleton-loader.js";
 import { useTheme } from "../../../design-system/theme-provider.js";
@@ -46,20 +47,17 @@ const AppPage = ({ owner, appName }: { owner: string; appName: string }) => {
 
   return (
     <div className="overflow-auto">
-      <div className={clsx("border-b", theme.border4, theme.bg4)}>
-        <div className="w-full max-w-7xl overflow-auto mx-auto p-4 md:p-8 flex">
-          <div className="space-y-1 flex-grow truncate">
-            <div className={clsx("text-2xl font-semibold", theme.text1)}>
-              {appName}
-            </div>
-            <div className={clsx("text-sm w-full truncate h-5", theme.text3)}>
-              {app.isLoading && (
-                <SkeletonLoader className="h-5 w-1/3" loading />
-              )}
-              {!app.isLoading && app.data?.app.description}
-            </div>
-          </div>
-          <div className="flex justify-end items-end gap-x-2">
+      <PageHeader
+        title={appName}
+        description={
+          app.isLoading ? (
+            <SkeletonLoader className="h-5 w-1/3" loading />
+          ) : (
+            app.data?.app.description
+          )
+        }
+        actions={
+          <>
             <a href={repoUrl} target="_blank">
               <Button className="truncate" disabled={!repoUrl}>
                 Git Repository
@@ -72,9 +70,9 @@ const AppPage = ({ owner, appName }: { owner: string; appName: string }) => {
             >
               Settings
             </Button>
-          </div>
-        </div>
-      </div>
+          </>
+        }
+      />
 
       <div className="w-full max-w-7xl overflow-auto mx-auto px-4 py-2 md:px-8 md:py:4">
         {owner && appName && (
--- apps/@wingcloud/website/src/routes/[owner]/[appName]/settings/index.tsx
+++ apps/@wingcloud/website/src/routes/[owner]/[appName]/settings/index.tsx
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 
 import { ErrorBoundary } from "../../../../components/error-boundary.js";
 import { Header } from "../../../../components/header.js";
+import { PageHeader } from "../../../../components/page-header.js";
 import { Button } from "../../../../design-system/button.js";
 import { Input } from "../../../../design-system/input.js";
 import { useNotifications } from "../../../../design-system/notification.js";
@@ -62,18 +63,7 @@ const SettingsPage = ({
 
   return (
     <>
-      <div className={clsx("border-b", theme.border4, theme.bg4)}>
-        <div className="w-full max-w-7xl overflow-auto mx-auto p-4 md:p-8 flex">
-          <div className="space-y-1 flex-grow truncate">
-            <div className={clsx("text-2xl font-semibold", theme.text1)}>
-              Project Settings
-            </div>
-            <div className={clsx("text-sm w-full truncate h-5", theme.text3)}>
-              {appName}
-            </div>
-          </div>
-        </div>
-      </div>
+      <PageHeader title="Project Settings" description={appName} />
       <div className="w-full max-w-7xl overflow-auto mx-auto p-4 md:p-8 relative">
         <div className="flex-grow space-y-4">
           <div
