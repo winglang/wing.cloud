8260c0af00cacfe9a68fbddeabd95b1588d83daa
polamoros <info@polamoros.com>
2024-02-13 17:47:35 +0100

    wip

--- apps/@wingcloud/website/src/routes/[owner]/[appName]/overview.tsx
+++ apps/@wingcloud/website/src/routes/[owner]/[appName]/overview.tsx
@@ -126,23 +126,22 @@ const OverviewPage = ({
             navigate(`/${owner}/${appName}/${productionEnvironment.branch}`);
           }}
           actions={
-            <Link
-              to={`/${owner}/${appName}/${productionEnvironment?.branch}`}
-              onClick={(event) => {
-                if (productionEnvironment?.status !== "running") {
-                  event.preventDefault();
-                }
-              }}
-              className={clsx(
-                theme.text2,
-                theme.text3Hover,
-                theme.bg4Hover,
-                "transition-all",
-                "z-10 rounded-full p-1.5",
+            <>
+              {productionEnvironment?.branch === "running" && (
+                <Link
+                  to={`/${owner}/${appName}/${productionEnvironment.branch}`}
+                  className={clsx(
+                    theme.text2,
+                    theme.text3Hover,
+                    theme.bg4Hover,
+                    "transition-all",
+                    "z-10 rounded-full p-1.5",
+                  )}
+                >
+                  <ArrowTopRightOnSquareIcon className="w-4 h-4" />
+                </Link>
               )}
-            >
-              <ArrowTopRightOnSquareIcon className="w-4 h-4" />
-            </Link>
+            </>
           }
         />
       </div>
