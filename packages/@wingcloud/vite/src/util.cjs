a78c174c5711311b98cedc891c464a0e297be9e2
Cristian Pallarés <cristianp@wing.cloud>
2024-02-15 16:25:01 +0100

    fix(vite): environment variable are not resolved during deploy

--- packages/@wingcloud/vite/src/util.cjs
+++ packages/@wingcloud/vite/src/util.cjs
@@ -23,3 +23,26 @@ exports.spawn = (options) => {
 exports.getURLFromText = (url) => {
   return url.match(/(https?:\/\/[^\s]+)/)?.[0];
 };
+
+exports.recursiveReadDir = (dir, files = []) => {
+  const fs = require("node:fs");
+  const path = require("node:path");
+
+  fs.readdirSync(dir).forEach((file) => {
+    const filePath = path.join(dir, file);
+    const stat = fs.lstatSync(filePath);
+    if (stat.isDirectory()) {
+      files = exports.recursiveReadDir(filePath, files);
+    } else {
+      files.push(filePath);
+    }
+  });
+
+  return files;
+};
+
+const { contentType } = require("mime-types");
+const { extname } = require("node:path");
+exports.contentType = (filename) => {
+  return contentType(extname(filename));
+};
