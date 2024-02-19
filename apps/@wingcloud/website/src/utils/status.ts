ac11485cfd7f8e70dcefbd2418e62e3d576f1256
Pol Amorós <info@polamoros.com>
2024-02-19 19:50:43 +0100

    feat: prevent loading the console if the environment is not running (#531)

--- apps/@wingcloud/website/src/utils/status.ts
+++ apps/@wingcloud/website/src/utils/status.ts
@@ -3,6 +3,23 @@ import { useMemo } from "react";
 import type { EnvironmentStatus } from "./wrpc.js";
 
 export const useStatus = (status?: EnvironmentStatus) => {
+  const color = useMemo(() => {
+    switch (status) {
+      case "initializing":
+      case "running-server":
+      case "running-tests":
+      case "deploying":
+        return "yellow";
+      case "running":
+        return "green";
+      case "error":
+      case "tests-error":
+        return "red";
+      case "stopped":
+        return "gray";
+    }
+  }, [status]) as "yellow" | "green" | "red" | "gray";
+
   const statusString = useMemo(() => {
     if (status === "running-server") {
       return "Starting";
@@ -10,11 +27,17 @@ export const useStatus = (status?: EnvironmentStatus) => {
     if (status === "running-tests") {
       return "Running Tests";
     }
+    if (status === "tests-error") {
+      return "Tests Error";
+    }
     if (status === "initializing" || status === "deploying") {
       return "Deploying";
     }
     return status;
   }, [status]);
 
-  return statusString;
+  return {
+    statusString,
+    color,
+  };
 };
