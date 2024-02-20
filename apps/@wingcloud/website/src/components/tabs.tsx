35315664737ceadf6a7f5cd9f66c865be72b4e34
polamoros <info@polamoros.com>
2024-02-20 09:34:49 +0100

    wip

--- apps/@wingcloud/website/src/components/tabs.tsx
+++ apps/@wingcloud/website/src/components/tabs.tsx
@@ -2,7 +2,6 @@ import clsx from "clsx";
 import { useMemo } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
-import { Tab as HeadlessTab } from "@headlessui/react";
 import { useTheme } from "../design-system/theme-provider.js";
 import { Link } from "react-router-dom";
 
@@ -35,35 +34,32 @@ export const Tabs = ({ tabs }: TabsProps) => {
   };
 
   return (
-    <HeadlessTab.Group onChange={changeTab}>
-      <HeadlessTab.List className="flex space-x-2">
-        {tabs.map((tab) => (
-          <div key={tab.name} className="relative pb-2">
-            <HeadlessTab
-              as={Link}
-              to={tab.to}
+    <nav className="flex space-x-2">
+      {tabs.map((tab) => (
+        <li key={tab.name} className="relative pb-2 list-none">
+          <Link
+            to={tab.to}
+            className={clsx(
+              "rounded-md px-2 py-1 text-sm font-medium",
+              "transition-all",
+              current?.name === tab.name && [theme.text1, theme.text1Hover],
+              current?.name !== tab.name && [theme.text3, theme.text3Hover],
+              theme.bgInput,
+              theme.focusVisible,
+            )}
+          >
+            {tab.name}
+            <div
               className={clsx(
-                "rounded-md px-2 py-1 text-sm font-medium",
-                "transition-all",
-                current?.name === tab.name && [theme.text1, theme.text1Hover],
-                current?.name !== tab.name && [theme.text3, theme.text3Hover],
-                theme.bgInput,
-                theme.focusVisible,
+                "absolute bottom-0 left-0 right-0 mx-1",
+                "pb-2",
+                "border-gray-600",
+                current?.name === tab.name && "border-b-2",
               )}
-            >
-              {tab.name}
-              <div
-                className={clsx(
-                  "absolute bottom-0 left-0 right-0 mx-1",
-                  "pb-2",
-                  "border-gray-600",
-                  current?.name === tab.name && "border-b-2",
-                )}
-              />
-            </HeadlessTab>
-          </div>
-        ))}
-      </HeadlessTab.List>
-    </HeadlessTab.Group>
+            />
+          </Link>
+        </li>
+      ))}
+    </nav>
   );
 };
