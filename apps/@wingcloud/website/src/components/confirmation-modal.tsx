604bae5a41e2863707f557d5f49b8f01b96c5fe3
polamoros <info@polamoros.com>
2024-03-16 11:22:06 +0100

    feat: add more data test ids

--- apps/@wingcloud/website/src/components/confirmation-modal.tsx
+++ apps/@wingcloud/website/src/components/confirmation-modal.tsx
@@ -67,6 +67,7 @@ export const ConfirmationModal = ({
       </div>
       <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
         <button
+          data-testid="modal-confirm-button"
           type="button"
           className={clsx(
             "inline-flex w-full justify-center rounded-md px-3 py-2 text-sm font-semibold shadow-sm sm:ml-3 sm:w-auto",
