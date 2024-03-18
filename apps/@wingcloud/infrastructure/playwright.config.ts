cdb020f7c16a6d1250f4e0e5c2c8aea444488fcf
polamoros <info@polamoros.com>
2024-03-18 12:38:06 +0100

    wip

--- apps/@wingcloud/infrastructure/playwright.config.ts
+++ apps/@wingcloud/infrastructure/playwright.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [["html", { open: "never" }]],
-  timeout: 60_000 * 5,
+  timeout: 60_000 * 2,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
