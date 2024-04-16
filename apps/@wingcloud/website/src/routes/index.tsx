1a83aaba22bb01cab950cd72c30d4f2ecad705d6
polamoros <info@polamoros.com>
2024-04-16 16:44:29 +0200

    wip

--- apps/@wingcloud/website/src/routes/index.tsx
+++ apps/@wingcloud/website/src/routes/index.tsx
@@ -1,26 +1,25 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useMemo } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 
 import { AuthDataProviderContext } from "../data-store/auth-data-provider.js";
+
+const ROUTES_WITHOUT_AUTH = new Set(["/", "/login"]);
 
 export const Component = () => {
   const navigate = useNavigate();
 
   const { error: authError } = useContext(AuthDataProviderContext);
 
+  const routeRequiresAuth = useMemo(
+    () => !ROUTES_WITHOUT_AUTH.has(location.pathname),
+    [location.pathname],
+  );
+
   useEffect(() => {
-    if (authError) {
+    if (routeRequiresAuth && authError) {
       navigate("/login");
     }
-  }, [authError]);
+  }, [routeRequiresAuth, authError]);
 
-  return (
-    <>
-      {location.pathname !== "/" && (
-        <>
-          <Outlet />
-        </>
-      )}
-    </>
-  );
+  return <>{(!routeRequiresAuth || !authError) && <Outlet />}</>;
 };
